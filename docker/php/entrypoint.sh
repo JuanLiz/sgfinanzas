208375b7dbbb7837ec341e4ddbf65d8cb41632f1
JuanLiz <47019693+JuanLiz@users.noreply.github.com>
2025-05-26 12:40:48 -0500

    remove artisan bs

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -14,17 +14,6 @@ echo "Verificando permisos..."
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Limpiar cache en caso de cambios
-php artisan optimize:clear
-
-# Optimizar Laravel
-echo "Optimizando Laravel..."
-php artisan optimize
-
-# Optimizar Filament
-echo "Optimizando Filament..."
-php artisan filament:optimize
-
 # Iniciar PHP-FPM
 echo "Iniciando PHP-FPM..."
 php-fpm
